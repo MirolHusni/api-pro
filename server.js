4e6c8cb5119c6e2604d9feb71db128a512d2c6ae
Glitch (api-pro) <none>
2018-03-26 04:27:00 +0000

    🍁🐓 Checkpoint ./server.js:762226/4214

--- server.js
+++ server.js
@@ -2,42 +2,24 @@
 // where your node app starts
 
 // init project
-//Basic required import for NodeJS
-var express = require('express')
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require('express')
+const app = express()
 
-var app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/dateValues/:dateVal", (request, response) => {
+app.get("/:dateVal", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
-    
+  
   var dateVal = request.params.dateVal;
   
-  response.json({unix: dateVal});
-});
+  response.json();
+})
 
-
-// //Create an instance of express for our app and instantiate bodyParser and Cors
-// var app = module.exports = express();
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// //Get CALL to return JSON thats format natural and unix date
-// app.get('/dateValues/:dateVal',function(req,res,next){
-  
-//   res.sendFile(__dirname + '/views/index.html')
-//   var dateVal = req.params.dateVal;
-  
-//   res.json({unix: dateVal});
-// });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
