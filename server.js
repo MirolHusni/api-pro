5f40dd4b5662be5ef5bfe60564f0db75d7cc2cb7
Glitch (api-pro) <none>
2018-03-26 04:15:56 +0000

    📼🎽 Checkpoint ./server.js:762226/1662

--- server.js
+++ server.js
@@ -7,19 +7,39 @@ var express = require('express')
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-//Create an instance of express for our app and instantiate bodyParser and Cors
-var app = module.exports = express();
+var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-//Get CALL to return JSON thats format natural and unix date
-app.get('/dateValues/:dateVal',function(req,res,next){
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/dateValues/:dateVal", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
+    
+  var dateVal = request.params.dateVal;
   
-  var dateVal = req.params.dateVal;
-  
-  res.json({unix: dateVal});
+  response.json({unix: dateVal});
 });
 
-app.listen(3000, function(){
-  console.log("It's working");
-});
+
+// //Create an instance of express for our app and instantiate bodyParser and Cors
+// var app = module.exports = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// //Get CALL to return JSON thats format natural and unix date
+// app.get('/dateValues/:dateVal',function(req,res,next){
+  
+//   res.sendFile(__dirname + '/views/index.html')
+//   var dateVal = req.params.dateVal;
+  
+//   res.json({unix: dateVal});
+// });
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
