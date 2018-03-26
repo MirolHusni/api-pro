12ed5c93ed89352ff8913d9b2669c7ed747d6c98
Glitch (api-pro) <none>
2018-03-26 02:51:00 +0000

    ⛩🐑 Checkpoint ./server.js:762226/1532

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
-//Basic required import for NodeJS
+// server.js
+// where your node app starts
 
-var express = require('express');
+// init project
+const express = require('express')
 var bodyParser = require('body-parser');
 var cors = require('cors');
+const app = express()
 
-//Create an instance of express for our app and instantiate bodyParser and Cors
-var app = module.exports = express();
-app.user(bodyParser.json());
-app.use(cors());
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+// Simple in-memory store
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes",
+  "Hi there, this is AmmarTV"
+]
+
+app.get("/dreams", (request, response) => {
+  response.send(dreams)
+})
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", (request, response) => {
+  dreams.push(request.query.dream)
+  response.sendStatus(200)
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
