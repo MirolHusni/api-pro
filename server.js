7d2fb2f7606490ff16e6d84b51f146d0a3e2d5df
Glitch (api-pro) <none>
2018-03-26 03:22:53 +0000

    🏨🍁 Checkpoint ./server.js:762226/194

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+//Get CALL to return JSON thats format natural and unix date
+app.get('dateValues',function(req,res,next){
+  console.log("URL workss");
+});
+
 app.listen(3000, function(){
   console.log("It's working");
 });
